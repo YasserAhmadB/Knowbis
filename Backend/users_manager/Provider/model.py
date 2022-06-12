c96d989dbff5daf725a159700a9d5c5fbe63c09b
Baghdadi <Yasser_Baghdadi@hotmail.com>
2022-06-12 12:36:24 +0300

    0.7.3.1 [ADD] blank=True to Provider

--- Backend/users_manager/Provider/model.py
+++ Backend/users_manager/Provider/model.py
@@ -7,4 +7,4 @@ class Provider(models.Model):
     major = models.CharField(max_length=255)
     pic = models.ImageField(null=True)
     description = models.CharField(max_length=1255, null=True, blank=True)
-    certificate = models.FileField(null=True)
+    # certificate = models.FileField(null=True)
